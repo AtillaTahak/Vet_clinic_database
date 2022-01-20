bca47c2a4fc6977226b9873ac3d57f009ed0f084
Atilla Taha Kördüğüm <atillatahaa@gmail.com>
2022-01-20 14:02:06 +0300

    completed milestone2

--- data.sql
+++ data.sql
@@ -5,3 +5,18 @@ iNSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUE
 iNSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Pikachu','2021-01-01',1,false,15.04);
 iNSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Devimon','2021-05-12',5,true,11);
 
+/* 
+Insert the following data:
+Animal: His name is Charmander. He was born on Feb 8th, 2020, and currently weighs -11kg. He is not neutered and he has never tried to escape.
+Animal: Her name is Plantmon. She was born on Nov 15th, 2022, and currently weighs -5.7kg. She is neutered and she has tried to escape 2 times.
+Animal: His name is Squirtle. He was born on Apr 2nd, 1993, and currently weighs -12.13kg. He was not neutered and he has tried to 3 times.
+Animal: His name is Angemon. He was born on Jun 12th, 2005, and currently weighs -45kg. He is neutered and he has tried to escape once.
+Animal: His name is Boarmon. He was born on Jun 7th, 2005, and currently weighs 20.4kg. He is neutered and he has tried to escape 7 times.
+Animal: Her name is Blossom. She was born on Oct 13th, 1998, and currently weighs 17kg. She is neutered and she has tried to escape 3 times. */
+
+iNSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Charmander','2020-02-08',0,false,-11);
+iNSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Plantmon','2022-11-15',2,true,-5.7);
+iNSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Squirtle','1993-12-2',3,false,-12.13);
+iNSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Angemon','2005-06-12',1,true,-45);
+iNSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Boarmon','2005-06-07',7,true,20.4);
+iNSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Blossom','1998-01-13',3,true,17);
--- queries.sql
+++ queries.sql
@@ -30,3 +30,76 @@ select * from animals where not name='Gabumon';
 /* Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg) */
 
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+
+/* Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that species columns went back to the state before tranasction. */
+
+begin;
+update animals set species='unspecified';
+rollback;
+
+/* Inside a transaction:
+Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+Commit the transaction.
+Verify that change was made and persists after commit. */
+
+Begin;
+update animals set species = 'digimon' where name like '%mon';
+update animals set species = 'pokemon' where species is NULL;
+COMMIT;
+
+/* Now, take a deep breath and... Inside a transaction delete all records in the animals table, then roll back the transaction.
+ */
+Begin;
+Delete from animals;
+Rollback;
+
+/* After the roll back verify if all records in the animals table still exist. After that you can start breath as usual ;) */
+
+select * from animals;
+
+
+/* Inside a transaction:
+Delete all animals born after Jan 1st, 2022.
+Create a savepoint for the transaction.
+Update all animals' weight to be their weight multiplied by -1.
+Rollback to the savepoint
+Update all animals' weights that are negative to be their weight multiplied by -1.
+Commit transaction */
+
+
+begin;
+delete from animals where date_of_birth>'2022-06-01';
+savepoint delete_date;
+update animals set weight_kg = weight_kg*-1;
+rollback to delete_date;
+update animals set weight_kg = weight_kg*-1 where weight_kg<0;
+ commit;
+
+
+
+/* How many animals are there?
+ */
+ select count(*) from animals ;
+
+/*  How many animals have never tried to escape? */
+
+ select count(*) from animals where escape_attempts=0;
+
+/*  What is the average weight of animals?
+ */
+
+ select avg(weight_kg) from animals;
+
+
+/* Who escapes the most, neutered or not neutered animals?
+ */
+  SELECT name, escape_attempts FROM animals WHERE escape_attempts = (SELECT MAX(escape_attempts) FROM animals);
+
+/* What is the minimum and maximum weight of each type of animal? */
+  select min(weight_kg),max(weight_kg),species from animals group by species;
+
+/* What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+ */
+ select avg(escape_attempts),species from animals where date_of_birth between '1990-01-01' and '2000-01-01' group by species;
